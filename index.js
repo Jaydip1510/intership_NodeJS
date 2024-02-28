e5cf2e0ee676c3da801de528e19f9d8d69d449c2
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2024-02-28 21:06:02 +0530

    changes

--- index.js
+++ index.js
@@ -12,21 +12,5 @@ const userModel = require('./models/user');
 
 app.use("/api/users",userRouter)
 
-// app.delete("/users/delete/:id", async (req, res) => {
-//     try {
-//         const id = req.params.id;
-//         const deletedUser = await userModel.findByIdAndDelete(id);
-
-//         if (!deletedUser) {
-//             return res.status(404).json({ message: "User not found" });
-//         }
-
-//         res.json({ message: "User deleted successfully", deletedUser });
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ error: 'Internal Server Error' });
-//     }
-// });
-
 dbConnection();
 app.listen(port,() => console.log('server is running on port'+port));
